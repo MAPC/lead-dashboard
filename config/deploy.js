38c2b0a30c83c441f6676fade045757b049bc632
Eric Youngberg <eyoungberg@mapc.org>
2018-11-30 10:41:22 -0500

    Update data. Add deploy methods.

--- config/deploy.js
+++ config/deploy.js
@@ -1,28 +1,30 @@
 /* jshint node: true */
+'use strict';
 
 module.exports = function(deployTarget) {
-  var ENV = {
-    build: {}
-    // include other plugin configuration that applies to all deploy targets here
+  let ENV = {
+    build: {
+      environment: deployTarget,
+    },
+
+    'revision-data': {
+      type: 'git-commit',
+    },
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
-
   if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
+    ENV.rsync = {
+      dest: 'lead-dashboard@prep.mapc.org:/var/www/lead-dashboard',
+      delete: false,
+    };
   }
 
   if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
+    ENV.rsync = {
+      dest: 'lead-dashboard@live.mapc.org:/var/www/lead-dashboard',
+      delete: false,
+    };
   }
 
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
   return ENV;
 };
